623bfc5caf5a451cd231d092b71f38a263d4e051
mildk123 <mildk123@gmail.com>
2018-12-18 18:40:30 +0500

    Added

--- Screens/Company/addCompany.js
+++ Screens/Company/addCompany.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
 
-import { StyleSheet } from "react-native";
-import { Container, Form, Item, Input, Label, View } from "native-base";
-import { Button } from 'react-native-elements'
+import { StyleSheet, View, Text } from "react-native";
+import { Container, Form, Item, Input, Label } from "native-base";
+import { Button } from "react-native-elements";
+
+import DateTimePicker from "react-native-modal-datetime-picker";
 
 import Header from "../../Helper/Header";
 
-
 class Company extends Component {
-  addCompany = () => {
-    console.log(123123);
-  };
+  constructor() {
+    super();
+    this.state = {
+      isDateTimePickerVisible: false
+    };
+  }
 
   static navigationOptions = {
     header: null
+  };
+
+  _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+  _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+  _handleDatePicked = date => {
+    console.log(date)
+    // this.setState({
+    //   estDate: date
+    // });
+    this._hideDateTimePicker();
   };
 
   render() {
@@ -32,15 +48,29 @@ class Company extends Component {
         <View style={styles.container}>
           <Form>
             <Item stackedLabel>
-              <Label>Name of company</Label>
+              <Label>Company Name</Label>
               <Input />
             </Item>
 
-            {/* <View style={{height: 70}}>
-              <Label>Established Since</Label>
+            <View style={{ height: 70 }}>
+              <Text>Established Since : </Text>
+              {!this.state.estDate ? <Text>Please select date of establishment</Text> : <Text>{this.state.estDate}</Text>}
 
-              <Date />
-            </View> */}
+              <Button 
+              title="Select Date" 
+              onPress={this._showDateTimePicker} 
+              style={{width: 200}} 
+              />
+
+              <DateTimePicker
+                isVisible={this.state.isDateTimePickerVisible}
+                onConfirm={this._handleDatePicked}
+                onCancel={this._hideDateTimePicker}
+                mode="date"
+                datePickerModeAndroid="default"
+                is24Hour={true}
+              />
+            </View>
 
             <Item stackedLabel last>
               <Label>Timings</Label>
