9335f4f4edc73d7f2c87602408afd07f6e09ab42
mildk123 <mildk123@gmail.com>
2018-12-20 15:34:16 +0500

    Added List

--- Screens/Company/Address.js
+++ Screens/Company/Address.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
 
 import { StyleSheet, View } from "react-native";
-import { Location, Permissions } from 'expo';
-import { Button, Text } from "native-base";
+import {
+  Container,
+  Content,
+  List,
+  ListItem,
+  Text,
+  Left,
+  Body,
+  Right,
+  Thumbnail,
+  Button,
+  Spinner,
+  Icon
+} from "native-base";
+
+import { Location, Permissions } from "expo";
 
 class Address extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isLoaded: false,
+      isLoading: true,
+      kidlist: []
+    };
   }
 
   static navigationOptions = {
-    title: 'Select Location'
+    title: "Select Location"
   };
 
   componentDidMount() {
@@ -20,9 +38,9 @@ class Address extends Component {
 
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
+    if (status !== "granted") {
       this.setState({
-        errorMessage: 'Permission to access location was denied',
+        errorMessage: "Permission to access location was denied"
       });
     }
 
@@ -30,26 +48,62 @@ class Address extends Component {
     this.setState({ location });
   };
 
-
   searchVenues = () => {
-    let searchQuery = 'khan';
+    let searchQuery = "khan";
 
-    fetch(`https://api.foursquare.com/v2/venues/search?query=${searchQuery}&limit=10&ll=24.9,67&client_id=0KBKAJO2Y4NWYLRXL4XDBKPEGG45EVHHIPKX5G32GTQGKNSS&client_secret=ZZOQY3QLIOGQ3FFHWQKZUIIH0KKTLQPOHFON001XQQDUEQOS&v=20181024`)
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      console.log(data.response)
-    })
-    .catch(error => error)
-    };
+    fetch(
+      `https://api.foursquare.com/v2/venues/search?query=${searchQuery}&limit=15&ll=24.9,67&client_id=0KBKAJO2Y4NWYLRXL4XDBKPEGG45EVHHIPKX5G32GTQGKNSS&client_secret=ZZOQY3QLIOGQ3FFHWQKZUIIH0KKTLQPOHFON001XQQDUEQOS&v=20181024`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        let dataX = data.response.venues;
 
+        this.setState({
+          venuesList: dataX,
+          isLoaded: true,
+          isLoading: false
+        });
+      })
+      .catch(error => error);
+  };
 
   render() {
+    const { isLoading, isLoaded, venuesList } = this.state;
+    if (isLoading) return <Spinner />;
+    if (!isLoaded || !venuesList) return null;
     return (
       <View style={styles.container}>
-        <Button><Text>search</Text></Button>
-        
+        <Container>
+          <Content>
+            <List>
+              {venuesList &&
+                venuesList.map((venue, index) => {
+                  console.log(venue)
+                  return (
+                    <ListItem
+                      avatar
+                      onPress={() => this._viewMore(index)}
+                      key={index}
+                    >
+                      <Left>
+                      <Icon style={{fontSize:30}} active name="ios-pin" />
+                      </Left>
+                      <Body>
+                        <Text>{venue.name}</Text>
+                        <Text note>Addrress : {venue.location.formattedAddress[0]}, {venue.location.formattedAddress[2]}, {venue.location.formattedAddress[2]} </Text>
+                      </Body>
+
+                      <Right>
+                        <Text note>3:43 pm</Text>
+                      </Right>
+                    </ListItem>
+                  );
+                })}
+            </List>
+          </Content>
+        </Container>
       </View>
     );
   }
@@ -60,7 +114,6 @@ export default Address;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 25,
     backgroundColor: "#ffffff"
   }
 });
---
+++ asd
@@ -0,0 +1,18 @@
+
+{id: "55b28066498e3047846f7623", name: "KhanJi", location: {…}, categories: Array(1), referralId: "v-1545297550", …}
+categories: [{…}]
+hasPerk: false
+id: "55b28066498e3047846f7623"
+location:
+address: "F-44/2, Block-4 Scheme-5 Clifton, Karachi, Pakistan"
+cc: "PK"
+city: "کراچی"
+country: "پاکستان"
+distance: 10890
+formattedAddress: (3) ["F-44/2, Block-4 Scheme-5 Clifton, Karachi, Pakistan", "کراچی-74400", "پاکستان"]
+labeledLatLngs: [{…}]
+lat: 24.80739534361634
+lng: 67.03476697989423
+neighborhood: "Clifton"
+postalCode: "74400"
+state: "سندھ"
