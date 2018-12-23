87bdf27153dde79d495f40f68aebe715c1482642
mildk123 <mildk123@gmail.com>
2018-12-23 19:46:40 +0500

    Added count

--- Screens/Company/setToken.js
+++ Screens/Company/setToken.js
@@ -15,7 +15,9 @@ import {
   Input,
   Label,
   CheckBox,
-  ListItem
+  ListItem,
+  Left,
+  Right
 } from "native-base";
 import Header from "../../Helper/Header";
 
@@ -39,6 +41,15 @@ export default class setToken extends Component {
           goBack={true}
         />
         <Tabs>
+          <Tab
+            tabStyle={{ backgroundColor: "#00BF8C" }}
+            textStyle={{ color: "#fff" }}
+            activeTabStyle={{ backgroundColor: "#00BF8C" }}
+            activeTextStyle={{ color: "#fff", fontWeight: "normal" }}
+            heading="Update"
+          >
+            <TokenUpdate />
+          </Tab>
           <Tab
             tabStyle={{ backgroundColor: "#00BF8C" }}
             textStyle={{ color: "#fff" }}
@@ -87,40 +98,49 @@ class TokenCard extends Component {
                 checked={this.state.allowTokens}
               />
               <Body>
-                {this.state.allowTokens ? <Text>Add Tokens </Text> : <Text>No Tokens today </Text>}
+                {this.state.allowTokens ? (
+                  <Text>Add Tokens </Text>
+                ) : (
+                  <Text>No Tokens today </Text>
+                )}
               </Body>
             </ListItem>
 
-            {this.state.allowTokens && <CardItem>
-              <Label>Total Tokens : </Label>
-              <Item style={{ width: 200 }}>
-                <Input 
-                  keyboardType="number-pad"
-                  onChangeText={text => this.setState({ companyName: text })}
-                />
-                <Icon active name="ios-help-buoy" />
-              </Item>
-            </CardItem>}
+            {this.state.allowTokens && (
+              <CardItem>
+                <Label>Total Tokens : </Label>
+                <Item style={{ width: 200 }}>
+                  <Input
+                    keyboardType="number-pad"
+                    onChangeText={text => this.setState({ companyName: text })}
+                  />
+                  <Icon active name="ios-help-buoy" />
+                </Item>
+              </CardItem>
+            )}
 
-            {this.state.allowTokens && <CardItem>
-              <Label>Time for each Token : </Label>
-              <Item style={{ width: 150 }}>
-                <Input
-                  keyboardType="number-pad"
-                  onChangeText={text => this.setState({ companyName: text })}
-                />
-                <Icon active name="md-time" />
-              </Item>
-            </CardItem>}
+            {this.state.allowTokens && (
+              <CardItem>
+                <Label>Time for each Token : </Label>
+                <Item style={{ width: 150 }}>
+                  <Input
+                    keyboardType="number-pad"
+                    onChangeText={text => this.setState({ companyName: text })}
+                  />
+                  <Icon active name="md-time" />
+                </Item>
+              </CardItem>
+            )}
 
-            {this.state.allowTokens && <CardItem style={{ justifyContent: "flex-end" }}>
-              <Button
-                style={{ backgroundColor: "#00BF8C", borderRadius: 25 }}
-              >
-                <Text>Done</Text>
-              </Button>
-            </CardItem>}
-
+            {this.state.allowTokens && (
+              <CardItem style={{ justifyContent: "flex-end" }}>
+                <Button
+                  style={{ backgroundColor: "#00BF8C", borderRadius: 25 }}
+                >
+                  <Text>Done</Text>
+                </Button>
+              </CardItem>
+            )}
           </Card>
         </Content>
       </View>
@@ -142,9 +162,76 @@ class Statistics extends Component {
   }
 }
 
+class TokenUpdate extends Component {
+  static navigationOptions = {
+    header: null
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, padding: 15 }}>
+        <Card style={{ padding: 2 }}>
+          <CardItem>
+            <Left>
+              <Label>ETA next Token : </Label>
+              <Item>
+                <Text> 15</Text>
+              </Item>
+            </Left>
+          
+
+          <Left>
+            <Label>Remaining : </Label>
+            <Item>
+              <Text> 15</Text>
+            </Item>
+          </Left>
+          </CardItem>
+        </Card>
+
+        <Card>
+          <View
+            style={{
+              padding: 25,
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <Text style={{ fontSize: 180, color: "#00BF8C" }}>05</Text>
+          </View>
+
+          <View style={styles.btnContainer}>
+            <Button
+              onPress={() => this.loginFB()}
+              iconRight
+              large
+              style={{
+                backgroundColor: "#00BF8C",
+                borderColor: "transparent",
+                borderWidth: 0,
+                borderRadius: 15
+              }}
+            >
+              <Text>Done</Text>
+            </Button>
+          </View>
+        </Card>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#ffffff"
+    backgroundColor: "#ffffff",
+    justifyContent: "flex-start"
+  },
+  btnContainer: {
+    height: 150,
+    padding: 25,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    justifyContent: "space-evenly"
   }
 });
